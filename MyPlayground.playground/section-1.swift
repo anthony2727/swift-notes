405129388e69152dbfb131b4d7a181a2bd09793a
=Anthony Rodriguez <=AnthonyRodriguez.itt@gmail.com>
2015-03-21 15:43:32 -0400

    Commenting first seccion

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -1,5 +1,58 @@
 // Playground - noun: a place where people can play
+/*
+    Author: Anthony Rodriguez. 
+    https://www.github.com/anthony2727/swift-notes
+
+    Playground is a project that aim tho help learning the Swift programming language. 
+
+    In this part we're practicing variable declaration and String concatenation.
+*/
+//UIKit stands for: User Interface Kit.
+//UIKit is a library. 
 
 import UIKit
 
+
+//Creating a new variable.
+/*
+    Swift has two reserved word for variable declaration: var and let.
+
+*/
+var name = "Anthony"
+let lastname = "Rodriguez"
+/*
+    Variables can be of any type. The type of a variable is described by the nature of the data that it holds.
+    There are multiple type of data:
+    
+    * String
+    * int
+    * double 
+    * float
+    * more... 
+
+    Swift is not a typed language. It means that the variable can be declared or created without specifying the type of date that 
+    it will be containing. In this case the data itself defines the type of the variable. 
+
+    Said so, a variable can be implicitly or explicitly declared. 
+
+*/
+
+// The type is implicitly declared.
 var str = "Hello, playground"
+
+// The type is explicitly declared.
+var shape: String = "Triangle"
+
+// *The same using let*
+
+// The type is implicitly declared.
+var str2 = "Hello, playground"
+
+// The type is explicitly declared.
+var shape2: String = "Rectangle"
+
+
+//End of section. Thanks. 
+
+
+
